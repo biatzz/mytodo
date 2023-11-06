84615976ddf408b976833548908aea414dfe33d3
Utn2 <biatzz@gmail.com>
2023-11-06 17:08:46 +0100

    Test 3

--- C2_TodoList_Latest_F.py
+++ C2_TodoList_Latest_F.py
@@ -1,7 +1,7 @@
 # Custom Function Definition
 
 import os
-def get_todos(filepath=r"C:\Users\Utente\Python\IdeaProjects\CourseNew\Files\todos.txt"):
+def get_todos(filepath="mytdo/todos.txt"):
 # def get_todos(filepath=os.path.abspath(r"FIles\todos.txt")):
    # os.path.abspath("todos.txt")
     with open(filepath, 'r') as file:
@@ -9,7 +9,7 @@ def get_todos(filepath=r"C:\Users\Utente\Python\IdeaProjects\CourseNew\Files\tod
     return todos_local
 
 # def write_todos(todos_arg, filepath=os.path.abspath(r"Files\todos.txt")):
-def write_todos(todos_arg, filepath=r"C:\Users\Utente\Python\IdeaProjects\CourseNew\Files\todos.txt"):
+def write_todos(todos_arg, filepath="mytdo/todos.txt"):
     with open(filepath, 'w') as file:
         todos = file.writelines(todos_arg)
 
